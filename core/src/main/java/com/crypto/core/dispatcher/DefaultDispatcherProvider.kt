bf9976d8f531e16f844d75c0d697c69f0d020f92
Thomas Ho <holuong1609@gmail.com>
2026-03-03 23:31:23 +0700

    build: add core module

---
+++ core/src/main/java/com/crypto/core/dispatcher/DefaultDispatcherProvider.kt
@@ -0,0 +1,9 @@
+package com.crypto.core.dispatcher
+
+import kotlinx.coroutines.Dispatchers
+
+class DefaultDispatcherProvider : DispatcherProvider {
+    override val io = Dispatchers.IO
+    override val main = Dispatchers.Main
+    override val default = Dispatchers.Default
+}
